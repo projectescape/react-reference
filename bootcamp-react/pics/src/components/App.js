871ec0234a7b9b8986552e0ec290179be3041a30
escapeReality <escaperealityproject@gmail.com>
2019-01-02 23:15:25 +0530

    Data fetching in react using axios/

--- bootcamp-react/pics/src/components/App.js
+++ bootcamp-react/pics/src/components/App.js
@@ -1,14 +1,26 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 950ffbd4f0e843eff45ebc782cbb76e841f1494a205ce1694ed04499e9b820d3"
+      }
+    });
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found : {this.state.images.length}
       </div>
     );
   }
