6391b83d1212fc9c5162a583b45b29608ddabc97
escapeReality <escaperealityproject@gmail.com>
2019-03-16 06:15:56 +0530

    Reconfigured webpack for same babel configs at server,client

--- server-side-react/server/build/bundle.js
+++ server-side-react/server/build/bundle.js
@@ -7026,16 +7026,32 @@ if (process.env.NODE_ENV === 'production') {
 "use strict";
 
 
-var express = __webpack_require__(53);
-var React = __webpack_require__(15);
-var renderToString = __webpack_require__(111).renderToString;
-var Home = __webpack_require__(122).default;
-var app = express();
+var _express = __webpack_require__(53);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(15);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(111);
+
+var _Home = __webpack_require__(122);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.use(_express2.default.static('public'));
 
 app.get('/', function (req, res) {
-  var content = renderToString(React.createElement(Home, null));
+  var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
-  res.send(content);
+  var html = '\n  <html>\n    <head>\n    </head>\n    <body>\n      <div id="root">\n        ' + content + '\n        <script src="bundle.js"></script>\n      </div>\n    </body>\n  </html>\n  ';
+
+  res.send(html);
 });
 
 app.listen(3000, function () {
@@ -22757,7 +22773,18 @@ var Home = function Home() {
   return _react2.default.createElement(
     'div',
     null,
-    'Home Component'
+    _react2.default.createElement(
+      'div',
+      null,
+      'Home Component'
+    ),
+    _react2.default.createElement(
+      'button',
+      { onClick: function onClick() {
+          return console.log('Hola');
+        } },
+      'Press here'
+    )
   );
 };
 
--- server-side-react/server/webpack.client.js
+++ server-side-react/server/webpack.client.js
@@ -3,14 +3,12 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 
 const config = {
-  // Inform webpack, building a bundle for nodejs, not browser
-  target: 'node',
   // Tell webpack root file of app
-  entry: './src/index.js',
+  entry: './src/client/client.js',
   // tell webpack where to put output file
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'public')
   }
 };
 
