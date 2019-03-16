aac42db82f51a7d8821a73db6c02f4659cf0e4ad
escapeReality <escaperealityproject@gmail.com>
2019-03-16 21:50:19 +0530

    Fetching data from api using client side

--- server-side-react/server/src/client/client.js
+++ server-side-react/server/src/client/client.js
@@ -1,13 +1,23 @@
 //  Startup point for client side application
+// To use some functions like async await
+import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 );
---
+++ server-side-react/server/src/helpers/createStore.js
@@ -0,0 +1,7 @@
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from '../client/reducers';
+export default () => {
+  const store = createStore(reducers, {}, applyMiddleware(thunk));
+  return store;
+};
