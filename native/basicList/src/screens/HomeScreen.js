1eb5a5c9373557a9de9c579f3db2e1eeefb4917f
aniket <escaperealityproject@gmail.com>
2019-12-27 02:33:32 +0530

    Butttons with navigation

--- native/basicList/src/screens/HomeScreen.js
+++ native/basicList/src/screens/HomeScreen.js
@@ -1,8 +1,31 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Lul Insaan!!</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Lul Insaan!!</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => {
+          navigation.navigate("Components");
+        }}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => {
+          navigation.navigate("List");
+        }}
+      />
+      {/* <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("List");
+        }}
+      >
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
