5061ece4ad38ab3ab984c2b61c4375431a9ff4b9
aniket <escaperealityproject@gmail.com>
2019-12-30 23:16:16 +0530

    Created ResultsShowScreen

--- native/food/src/components/ResultsList.js
+++ native/food/src/components/ResultsList.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
 import ResultsDetail from "./ResultsDetail";
+import { withNavigation } from "react-navigation";
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) return null;
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
@@ -14,7 +23,15 @@ const ResultsList = ({ title, results }) => {
           return id;
         }}
         renderItem={({ item }) => {
-          return <ResultsDetail result={item} />;
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("ResultsShow", { id: item.id });
+              }}
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
         }}
       />
     </View>
@@ -31,4 +48,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
