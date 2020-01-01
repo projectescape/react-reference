21ab68a0bed2abb3b426da65fa315f71a86384c2
aniket <escaperealityproject@gmail.com>
2020-01-01 22:32:31 +0530

    Added CreateScreen to add blogs

--- native/blog/src/screens/CreateScreen.js
+++ native/blog/src/screens/CreateScreen.js
@@ -1,15 +1,53 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 import { Context } from "../context/BlogContext";
 
 const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const { addBlogPost } = useContext(Context);
+
   return (
     <View>
-      <Text>CreateScreen</Text>
+      <Text style={styles.label}>Enter Title :</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={text => {
+          setTitle(text);
+        }}
+      />
+      <Text style={styles.label}>Enter Content :</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={text => {
+          setContent(text);
+        }}
+      />
+      <Button
+        title="Add BlogPost"
+        onPress={() => {
+          addBlogPost(title, content, () => {
+            navigation.navigate("Index");
+          });
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderColor: "black",
+    borderWidth: 1,
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
+  },
+  label: { fontSize: 20, marginBottom: 5, marginLeft: 5 }
+});
 
 export default CreateScreen;
