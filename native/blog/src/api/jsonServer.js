7cb764ce2b9b08614f0ec9e2328e3bc4d3c00dc4
aniket <escaperealityproject@gmail.com>
2020-01-02 22:10:23 +0530

    Initialized json-server with ngrok for blogs database

---
+++ native/blog/src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://4e20bb98.ngrok.io"
+});
--- native/blog/src/context/BlogContext.js
+++ native/blog/src/context/BlogContext.js
@@ -1,8 +1,11 @@
-import React, { useReducer } from "react";
 import createDataContext from "./createDataContext";
+import jsonServer from "../api/jsonServer";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "get_blogposts":
+      return action.payload;
+
     case "add_blogpost":
       return [
         ...state,
@@ -24,6 +27,14 @@ const blogReducer = (state, action) => {
       return state;
   }
 };
+
+const getBlogPosts = dispatch => {
+  return async () => {
+    const res = await jsonServer.get("/blogposts");
+    dispatch({ type: "get_blogposts", payload: res.data });
+  };
+};
+
 const addBlogPost = dispatch => {
   return (title, content, callback) => {
     dispatch({ type: "add_blogpost", payload: { title, content } });
@@ -46,6 +57,6 @@ const editBlogPost = dispatch => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{ title: "Test Post", content: "Test Content", id: 1 }]
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+  []
 );
